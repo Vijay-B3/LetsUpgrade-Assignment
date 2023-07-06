493927ea5e620bedec8d5267efbd833129204a37
Vijay-B3 <136103873+Vijay-B3@users.noreply.github.com>
2023-07-06 15:03:00 +0530

    Day 3 Assignment submitted

---
+++ Day 3.py
@@ -0,0 +1,7 @@
+def remove_vowels(string):
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    result = ""
+    for char in string:
+        if char not in vowels:
+            result += char
+    return result
